b8475f8ff4ebd13efe1e69f787ffef5075b328da
Rashi Jha <jhar7605@gmail.com>
2025-01-26 14:46:44 -0600

    practice.py file added

---
+++ practice.py
@@ -0,0 +1,16 @@
+
+thislist=[1,2,3,4,5,6,7,8,9,10]
+nameList= ["Abby", "Kayla", "Rashi", "Rishi"]
+
+for x in nameList:
+    if "A" in x or "R" in x :
+        print(x)
+
+
+print("Kayla" in nameList)
+
+evenList=[num for num in thislist if num%2==0] 
+i=0
+while i<len(evenList):
+    print(evenList[i])
+    i=i+1
