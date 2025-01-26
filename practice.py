d15682a461c1e48cb7af6108f2f1bb9c2e281408
Rashi Jha <jhar7605@gmail.com>
2025-01-26 14:54:57 -0600

    New changes made to practice.py

--- practice.py
+++ practice.py
@@ -1,4 +1,5 @@
 
+import random
 thislist=[1,2,3,4,5,6,7,8,9,10]
 nameList= ["Abby", "Kayla", "Rashi", "Rishi"]
 
@@ -14,3 +15,8 @@ i=0
 while i<len(evenList):
     print(evenList[i])
     i=i+1
+
+
+
+print(thislist[-4:-1])#print 7,8,9 since -1 refers to the last element(work to the left)
+print(nameList[random.randrange(0, len(nameList))])
